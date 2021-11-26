cd282a347e850b3d833ac48c31164c36dee1df93
stephanie ezimoha <stephanie.ezimoha@yahoo.co.uk>
2021-11-26 04:00:03 +0000

    Added second dice to pig game

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const current0l = document.getElementById('current--0')
 const current1l = document.getElementById('current--1')
 
 
-const dicel = document.querySelector('.dice');
+const dice1 = document.getElementById('dice-1');
+const dice2 = document.getElementById('dice-2');
 const btnNewGame = document.querySelector('.btn--new')
 const btnRollDice = document.querySelector('.btn--roll')
 const btnholdScore = document.querySelector('.btn--hold')
@@ -28,7 +29,10 @@ const initialiseGame = function(){
   score1l.textContent = 0;
   current0l.textContent = 0;
   current1l.textContent = 0;
-  dicel.classList.add('hidden');
+  // dice1.classList.add('hidden');
+  // dice2.classList.add('hidden');
+   document.getElementById('dice-1').style.display = 'none';
+   document.getElementById('dice-2').style.display = 'none';
 
   player0l.classList.remove('player--winner');
   player1l.classList.remove('player--winner');
@@ -42,21 +46,30 @@ btnRollDice.addEventListener('click', function () {
   if (playing) {
     // 1. Generate random roll
 
-    const dice = Math.trunc(Math.random() * 6) + 1;
+    const dice1 = Math.trunc(Math.random() * 6) + 1;
+    const dice2 = Math.trunc(Math.random() * 6) + 1;
     // console.log(dice);
 
     // 2. Display dice
 
-    dicel.classList.remove('hidden');
-    dicel.src = `dice-${dice}.png`;
+    // dice1.classList.remove('hidden');
+    // dice2.classList.remove('hidden');
+    // dice1.src = `dice-${dice1}.png`;
+    // dice2.src = `dice-${dice2}.png`;
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
+    console.log(dice1)
+    console.log(dice2)
 
     // 3. Check for dice-1, if true, switch to next player.
-    if (dice != 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add dice to current store
-      currentScore += dice;
+      currentScore += dice1 + dice2;
       document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
-      //current0l.textContent = currentScore;
+      // current0l.textContent = currentScore;
     } else {
       // switch player
       document.getElementById(`current--${currentPlayer}`).textContent = 0;
@@ -79,7 +92,8 @@ btnholdScore.addEventListener('click', function () {
   if (scores[currentPlayer] >= 100) {
     // Finish game
     playing = false;
-    dicel.classList.add('hidden');
+    // dice1.classList.add('hidden');
+    // dice2.classList.add('hidden');
     document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
     document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
   } else {
